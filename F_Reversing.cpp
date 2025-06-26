b3e6d3fe16b2d85ced9f36bf905045d3a20dece1
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-26 22:15:53 +0600

    vector reversing

---
+++ F_Reversing.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    reverse(v.begin(), v.end());
+
+    for (int x : v)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
+
+// Time complexity: O(N)
